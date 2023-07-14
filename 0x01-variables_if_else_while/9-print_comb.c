a4b587cb43ebf0aea6f6e2cc0a561eeb645fb291
Kayode Ogidan <kdogidan@gmail.com>
2023-07-14 05:49:16 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,20 +6,18 @@
  * main - A  program that prints all possible combinations of single-digit
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-        int d;
-        for (d = '0'; d <= '9'; d++)
-	{
-	putchar(d);
-	if (d != '9')
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-        putchar('\n');
-        return(0);
+int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
