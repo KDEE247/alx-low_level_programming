33987ac34e1c89d5bc264b6c155d8ed0a49bda25
Kayode Ogidan <kdogidan@gmail.com>
2023-07-14 07:01:22 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - A program that prints the letters of the alphabet except q and e
- * Return: Always 0 (Success)
+ *main - A program that prints the letters of the alphabet except q and e
+ *Return: Always 0 (Success)
  */
-
 int main(void)
-        
-{       
-        char low, e, q;
-
-	e = 'e';
-	q = 'q';
-        
-        for (low = 'a'; low <= 'z'; low++)
-	{
-	if (low != e && low != q)	
-        putchar(low);
-	}
-        putchar('\n');
-        return (0);
-        
-}     
-
+{
+char low, e, q;
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
+putchar('\n');
+return (0);
+}
