66fd635dd1706898e9ba9eefd67e555d8485f090
Kayode Ogidan <kdogidan@gmail.com>
2023-07-18 08:34:24 -0700

    3-islower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- *main - a program function that prints the alphabet, in lowercase
+ *main:print_alphabet - prints the alphabet, in lowercase
  *Return: Always 0
  */
-void print_alphabet(void)/*void print_alphabet(void) is a prototype*/
+voiid print_alphabet(void)
 {
-char c;
+/**
+ *void print_alphabet(void) - prints function
+*/
+
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
