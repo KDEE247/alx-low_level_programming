61d24d16f51b0c679a19203fde706a3f009c0088
KDEE247 <kdogidan@gmail.com>
2023-11-13 04:21:34 -0800

    shared lib

---
+++ 0x18-dynamic_libraries/operators.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * add - Add 2 Integers.
+ * @a: 1st Int.
+ * @b: 2nd Int.
+ * Return: Addition.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtract 2 Integers.
+ * @a: 1st Int.
+ * @b: 2nd Int.
+ * Return: Subtraction.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * Mod - Mod 2 Integers.
+ * @a: 1st Int.
+ * @b: 2nd Int.
+ * Return: Result.
+ */
+int mod(int a, int b)
+{
+	return (a %  b);
+}
+
+/**
+ * mul - Multiply 2 Integers.
+ * @a: 1st Int.
+ * @b: 2nd Int.
+ * Return: Multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a *  b);
+}
+
+/**
+ * div - Divide 2 Integers.
+ * @a: 1st Int.
+ * @b: 2nd Int.
+ * Return: Division.
+ */
+int div(int a, int b)
+{
+	return (a %  b);
+}
