63c0633e13f95279d996d146d35d66d47a04b471
KDEE247 <kdogidan@gmail.com>
2023-11-13 08:19:24 -0800

    edits

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P .. https://raw.github.usercontent.com/KDEE247/alx-low_level_programming/master/0x18-dynamic_libraries/libhacks.so
+wget -P .. https://raw.githubusercontent.com/KDEE247/alx-low_level_programming
+/master/0x18-dynamic_libraries/libhacks.so
 export LD_PRELOAD="$PWD/../libhacks.so"
