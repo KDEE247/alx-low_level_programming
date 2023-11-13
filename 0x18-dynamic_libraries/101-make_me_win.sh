cb627aae96ad547149a879426a1146ac6219afac
KDEE247 <kdogidan@gmail.com>
2023-11-13 08:23:31 -0800

    tweak

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/KDEE247/alx-low_level_programming
+wget -P .. https://raw.githubusercontent.com/KDEE247/alx-low_level_programming_
 /master/0x18-dynamic_libraries/libhacks.so
 export LD_PRELOAD="$PWD/../libhacks.so"
